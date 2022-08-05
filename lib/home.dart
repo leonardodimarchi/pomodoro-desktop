c51e754b04151b480c66bd63a1e4b756a062bf35
Leonardo Dimarchi <leonardodimarchi@outlook.com>
2022-08-05 20:15:30 -0300

    feature(home): added timer and some styling

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:pomodoro_desktop/widgets/window_title_bar.dart';
 
@@ -9,22 +11,79 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int secondsAmount = 25 * 60;
+  bool isAtBreak = false;
+  Timer? timer;
+
+  void toggleTimer() {
+    if (timer != null && timer!.isActive) {
+      pause();
+
+      if (isAtBreak) {
+        setState(() {
+          secondsAmount = 25 * 60;
+          isAtBreak = false;
+        });
+      } else {
+        setState(() {
+          secondsAmount = 5 * 60;
+          isAtBreak = false;
+        });
+      }
+    } else {
+      startTimer();
+    }
+  }
+
+  void startTimer() {
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        secondsAmount--;
+      });
+    });
+  }
+
+  void pause() {
+    timer?.cancel();
+  }
+
   @override
   Widget build(BuildContext context) {
+    int minutes = (secondsAmount / 60).floor();
+    int seconds = secondsAmount % 60;
+
+    String showingTimer =
+        '${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}';
+
     return Scaffold(
       body: Column(
         children: [
           const WindowTitleBar(),
-          Column(
+          Expanded(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              const Text('25:00'),
+              Text(
+                showingTimer,
+                style: Theme.of(context)
+                    .textTheme
+                    .displayLarge
+                    ?.copyWith(fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 30),
               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  TextButton(onPressed: () {}, child: const Text('Start')),
+                  ElevatedButton(
+                    onPressed: toggleTimer,
+                    child: Text(
+                        (timer != null && timer!.isActive) ? 'Stop' : 'Start'),
+                  ),
                 ],
               )
             ],
-          ),
+          ))
         ],
       ),
     );
